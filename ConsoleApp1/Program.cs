ea282ab05fc2c7d366d17b8542cebdaea687e851
Ertuğrul Baran <ert.baran@gmail.com>
2021-02-03 18:25:49 +0300

    Kontrollü son test yapıldı.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VehicleManager vehicleManager = new VehicleManager(new InMemoryVehicleDal());
+            foreach (var vehicle in vehicleManager.GetAll())
+            {
+                Console.WriteLine(vehicle.ModelYear+" Model"+", "+vehicle.BrandId+" Marka Kodlu Araç: "+ vehicle.DailyPrice+" TL");
+            }
+            Console.Read();
+        }
+    }
+}
